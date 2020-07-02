f00163af4857a485786a8d46b05cfcc6b73f6155
Ghiad <48151874+Ghiad@users.noreply.github.com>
2020-07-02 09:23:41 +0800

    my_vector

--- plan.h
+++ plan.h
@@ -3,7 +3,7 @@
 
 #include"pch.h"
 #include"controller.h"
-
+#include"my_vector.h"
 extern void makepath();
-extern void strategy(Passenger &, string, vector<string> &, list<Path> &,  double &, double &, vector<double> &, int, int, int);
+extern void strategy(Passenger &, string, my_vector<string> &,list<Path> &,  double &, double &, my_vector<double> &, int, int, int);
 #endif PLAN_H
